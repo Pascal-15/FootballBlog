90d9fa566b1ea8e5038e385d3cbe17acab602e55
Pascal-K <karsten.pas@gmail.com>
2024-05-22 21:09:37 +0200

    articles layout

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 const mode = document.getElementById('switch-mode');
 const html = document.getElementById('html');
+const containerDaily = document.getElementsByClassName('container-daily');
 
 
 
 //Top daily article
 const daily = {
-    path: 'assets/sounds/daily.jpg',
+    path: 'assets/img/daily.jpg',
     category: 'TRANSFERGERÜCHTE',
     headline: 'KOMPANY ZUM FC-BAYERN?',
     snippetText: 'Der FB-Bayern ist wohl an einer verpflichtung von Vincent Kompany als Trainer interessiert!',
     text: 'Hat die lange Trainersuche beim FC-Bayern bald endlich ein Ende. Anscheinend sollen die Bayern daran interessiert sein Vincent Kompany zu verpflichten.'
 }
+
+const articels = [
+    {
+    path: 'assets/img/tactics_1.png',
+    category: 'TAKTIKEN',
+    headline: 'WIE LÄSST XABI SPIELEN?',
+    snippetText: 'Der FB-Bayern ist wohl an einer verpflichtung von Vincent Kompany als Trainer interessiert!',
+    text: 'Hat die lange Trainersuche beim FC-Bayern bald endlich ein Ende. Anscheinend sollen die Bayern daran interessiert sein Vincent Kompany zu verpflichten.'
+    }
+]
 
 function darkMode() {
     if (mode.checked == true) {
@@ -21,5 +32,10 @@ function darkMode() {
     }
 }
 
+function loadArticle() {
+    
+}
+
 
 html.addEventListener('click', darkMode)
+containerDaily.addEventListener('click', loadArticle)
